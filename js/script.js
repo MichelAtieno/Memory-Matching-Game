d3ea29ca32b3a02d9c77acc7c2898c08e9b123c8
Michel Atieno <mishqamish@gmail.com>
2018-12-17 15:05:56 +0300

    Able to match correctly

--- js/script.js
+++ js/script.js
@@ -14,11 +14,31 @@ function flipCard(){
    } else {
        // second click
        hasFlippedCard = false;
-       secondCard = this;
-
-       console.log(firstCard.dataset.framework);
-       console.log(secondCard.dataset.framework);
+       secondCard = this; 
+       
+       checkForMatch()
    }
 }
 
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+    isMatch ? disableCards() : unflipCards();
+    
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+}
+
+function unflipCards() {
+    // not a match 
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+       }, 1500);
+
+}
 cards.forEach(card => card.addEventListener('click', flipCard))
